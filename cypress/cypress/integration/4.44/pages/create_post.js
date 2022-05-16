c5300d1a3f64660e3dca1fa003e59eb908477dfc
raficorr <rh.rodriguezr1@uniandes.edu.co>
2022-05-15 22:37:12 -0500

    Add files via upload

---
+++ cypress/cypress/integration/4.44/pages/create_post.js
@@ -0,0 +1,33 @@
+export class Page {
+   
+    
+    navigate_to_post_list(){
+        //.get('nav.gh-nav')
+        cy.get('a[href="#/pages/"]').eq(0).click()
+        cy.wait(2000)
+    }
+
+    click_to_create_new_post() {
+        
+        cy.get('a.gh-btn-primary[href="#/editor/page/"]').click();
+        cy.wait(3000)
+        return this;
+
+    }
+
+    create_post(pageTitle, pageNote, publish) {
+
+        // Required fields
+        cy.get('div.gh-koenig-editor').within(() => {
+            
+            if(postTitle)
+                cy.get('textarea.gh-editor-title').type(postTitle)
+            if(postNote)
+                cy.get('div.koenig-editor__editor').type(postNote) 
+            
+        })
+         
+    }
+}    
+
+
